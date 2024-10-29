8f4a7569e812e56a84dd9494347f2ba3c068a6b7
ramontrndd <134494678+ramontrndd@users.noreply.github.com>
2024-10-29 20:52:12 -0300

    Initialize backend with Express, TypeScript, and MySQL configuration

---
+++ src/config/connection.ts
@@ -0,0 +1,25 @@
+import mysql from 'mysql';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const dbConfig = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+};
+
+console.log('Tentando conectar ao banco de dados com a seguinte configuração:', dbConfig);
+
+const connection = mysql.createConnection(dbConfig);
+
+connection.connect((err: any) => {
+  if (err) {
+    console.error('Erro ao conectar ao MySQL:', err);
+    return;
+  }
+  console.log('Conexão com MySQL estabelecida com sucesso');
+});
+
+export default connection;
